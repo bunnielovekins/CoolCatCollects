8c9f20c345964c6a7b6cfe238c96c0b97762244d
Peter Storey <bunnielovekins@outlook.com>
2020-07-27 17:27:49 +0100

    More cleaning up and commenting

--- CoolCatCollects.Bricklink/BricklinkService.cs
+++ CoolCatCollects.Bricklink/BricklinkService.cs
@@ -9,6 +9,9 @@ using System.Web;
 
 namespace CoolCatCollects.Bricklink
 {
+	/// <summary>
+	/// Top-level class that deals with BL
+	/// </summary>
 	public class BricklinkService
 	{
 		private readonly BricklinkApiService _apiService;
@@ -20,6 +23,11 @@ namespace CoolCatCollects.Bricklink
 			_dataService = new BricklinkDataService();
 		}
 
+		/// <summary>
+		/// Gets a list of orders of a status. No order details
+		/// </summary>
+		/// <param name="status">Status to search for</param>
+		/// <returns>Model - list of orders, also status</returns>
 		public OrdersModel GetOrders(string status)
 		{
 			var result = _apiService.GetRequest($"orders?direction=in&status={status.ToUpper()}");
@@ -31,6 +39,10 @@ namespace CoolCatCollects.Bricklink
 			return model;
 		}
 
+		/// <summary>
+		/// Gets orders that aren't in the DB
+		/// </summary>
+		/// <returns>A Json object</returns>
 		public object GetOrdersNotInDb()
 		{
 			var result = _apiService.GetRequest($"orders?direction=in&status=shipped,received,completed");
@@ -54,6 +66,11 @@ namespace CoolCatCollects.Bricklink
 			return model;
 		}
 
+		/// <summary>
+		/// Gets messages for an order. Ignores feedback messages.
+		/// </summary>
+		/// <param name="orderId">Order Id</param>
+		/// <returns>BL Message Model</returns>
 		public IEnumerable<BricklinkMessage> GetOrderMessages(string orderId)
 		{
 			var result = _apiService.GetRequest<GetOrderMessagesResponse>($"orders/{orderId}/messages");
@@ -71,6 +88,11 @@ namespace CoolCatCollects.Bricklink
 			);
 		}
 
+		/// <summary>
+		/// Updates DB inventory for a colour by calling GetPartModel. Used only in DB update force.
+		/// </summary>
+		/// <param name="colourId">Colour ID</param>
+		/// <returns>List of errors</returns>
 		public IEnumerable<string> UpdateInventoryForColour(int colourId)
 		{
 			var result = _apiService.GetRequest<GetInventoriesResponseModel>("inventories?color_id=" + colourId);
@@ -91,96 +113,40 @@ namespace CoolCatCollects.Bricklink
 			return errors;
 		}
 
-		public int GetItemsRemaining(string inventoryId)
-		{
-			var result = _apiService.GetRequest("/inventories/" + inventoryId);
-
-			var responseModel = JsonConvert.DeserializeObject<GetInventoryResponseModel>(result);
-
-			if (responseModel.data == null)
-			{
-				result = _apiService.GetRequest("/inventories/" + inventoryId);
-
-				responseModel = JsonConvert.DeserializeObject<GetInventoryResponseModel>(result);
-
-				if (responseModel.data == null)
-				{
-					throw new Exception("An error has occurred! inventoryId: " + inventoryId + ", Error code: " + responseModel.meta.code +
-						", Error Message: " + responseModel.meta.message +
-						", Error Description: " + responseModel.meta.description);
-				}
-			}
-
-			return responseModel.data.quantity;
-		}
-
+		/// <summary>
+		/// Gets an order in a format for the CSV export
+		/// </summary>
+		/// <param name="orderId">Order Id</param>
+		/// <returns>OrderCsvModel for export</returns>
 		public OrderCsvModel GetOrderForCsv(string orderId)
 		{
-			var response = GetOrder(orderId);
+			var response = _apiService.GetRequest<GetOrderResponseModel>("orders/" + orderId);
 
 			var model = new OrderCsvModel(response);
 
 			return model;
 		}
 
-		public GetOrderResponseModel GetOrder(string orderId)
-		{
-			var result = _apiService.GetRequest("orders/" + orderId);
-
-			var responseModel = JsonConvert.DeserializeObject<GetOrderResponseModel>(result);
-
-			if (responseModel.data == null)
-			{
-				result = _apiService.GetRequest("orders/" + orderId);
-
-				responseModel = JsonConvert.DeserializeObject<GetOrderResponseModel>(result);
-
-				if (responseModel.data == null)
-				{
-					throw new Exception("An error has occurred! Order ID: " + orderId + ", Error code: " + responseModel.meta.code +
-						", Error Message: " + responseModel.meta.message +
-						", Error Description: " + responseModel.meta.description);
-				}
-			}
-
-			return responseModel;
-		}
-
-		public GetOrderItemsResponseModel GetOrderItems(string orderId)
-		{
-			var result = _apiService.GetRequest("orders/" + orderId + "/items");
-
-			var responseModel = JsonConvert.DeserializeObject<GetOrderItemsResponseModel>(result);
-
-			if (responseModel.data == null)
-			{
-				result = _apiService.GetRequest("orders/" + orderId + "/items");
-
-				responseModel = JsonConvert.DeserializeObject<GetOrderItemsResponseModel>(result);
-
-				if (responseModel.data == null)
-				{
-					throw new Exception("An error has occurred! Order ID: " + orderId + ", Error code: " + responseModel.meta.code +
-						", Error Message: " + responseModel.meta.message +
-						", Error Description: " + responseModel.meta.description);
-				}
-			}
-
-			return responseModel;
-		}
-
+		/// <summary>
+		/// Gets an order with its order items
+		/// </summary>
+		/// <param name="orderId">Order Id</param>
+		/// <returns>Model with order and items</returns>
 		public OrderWithItemsModel GetOrderWithItems(string orderId)
 		{
-			var order = GetOrder(orderId);
+			var order = _apiService.GetRequest<GetOrderResponseModel>("orders/" + orderId);
 
-			var orderItems = GetOrderItems(orderId);
+			var orderItems = _apiService.GetRequest<GetOrderItemsResponseModel>("orders/" + orderId + "/items");
 
+			// Add order to the DB
 			var orderEntity = _dataService.AddOrder(order, orderItems);
 
+			// Items come through in a very strange format, so get them from the DB after having just added them to the DB.
 			var items = orderItems.data
 				.SelectMany(x => x)
 				.Select(item =>
 				{
+					// Find this item in the DB
 					var itemEntity = orderEntity.OrderItems.FirstOrDefault(x => 
 						item.item.no == x.Part.Part.Number && 
 						item.color_id == x.Part.ColourId &&
@@ -201,17 +167,19 @@ namespace CoolCatCollects.Bricklink
 						UnitPrice = unitPrice,
 						TotalPrice = totalPrice,
 						Description = item.description,
-						Type = itemEntity.Part.Part.ItemType,
-						Weight = itemEntity.Part.Part.Weight,
+						Type = item.item.type,
+						Weight = item.weight,
 						ItemsRemaining = itemEntity.Part.Quantity,
 						Image = itemEntity.Part.Image
 					};
 
 					if (order.data.date_ordered > itemEntity.Part.LastUpdated)
 					{
+						// Update the inventory from the info we have in this order
 						_dataService.UpdatePartInventoryFromOrder(itemEntity.Part, item.remarks, item.unit_price_final, item.description, item.inventory_id);
 					}
 
+					// Works out some stuff related to the remarks and the location ordering
 					itemModel.FillRemarks();
 
 					return itemModel;
@@ -245,9 +213,14 @@ namespace CoolCatCollects.Bricklink
 			};
 		}
 
+		/// <summary>
+		/// Gets all the parts in a set
+		/// </summary>
+		/// <param name="set">Set number. Should already be checked to include -1</param>
+		/// <returns>A list of parts</returns>
 		public SubsetPartsListModel GetPartsFromSet(string set)
 		{
-			var responseModel = GetSubset(set);
+			var responseModel = _apiService.GetRequest<GetSubsetResponse>($"items/SET/{set}/subsets");
 
 			var model = new SubsetPartsListModel(responseModel);
 
@@ -276,24 +249,22 @@ namespace CoolCatCollects.Bricklink
 			return model;
 		}
 
-		public GetSubsetResponse GetSubset(string set, string type = "SET")
-		{
-			var result = _apiService.GetRequest<GetSubsetResponse>($"items/{type}/{set}/subsets");
-
-			return result;
-		}
-
+		/// <summary>
+		/// Gets details of a set; used for stock purchases. Mostly to do with value.
+		/// </summary>
+		/// <param name="set">Set number. -1 should already be there.</param>
+		/// <returns>Object for json</returns>
 		public object GetSetDetails(string set)
 		{
-			var setInfo = GetItem("SET", set);
+			var setInfo = _apiService.GetRequest<GetItemResponse>($"items/SET/{set}");
 
-			var categoryInfo = GetCategory(setInfo.data.category_id);
+			var categoryInfo = _apiService.GetRequest<GetCategoryResponse>($"categories/{setInfo.data.category_id}");
 
-			var parts = GetSubset(set);
+			var parts = _apiService.GetRequest<GetSubsetResponse>($"items/SET/{set}/subsets");
 
 			var minifigs = parts.data.SelectMany(x => x.entries).Where(x => x.item.type == "MINIFIG").Select(x => _dataService.GetPartModel(x.item.no, x.color_id, x.item.type, "N"));
 
-			var minifigParts = minifigs.Select(x => GetSubset(x.Part.Number, "MINIFIG")).
+			var minifigParts = minifigs.Select(x => _apiService.GetRequest<GetSubsetResponse>($"items/MINIFIG/{set}/subsets")).
 				SelectMany(x => x.data).
 				SelectMany(x => x.entries).
 				Select(x => new { model = _dataService.GetPartModel(x.item.no, x.color_id, x.item.type, "N"), quantity = x.quantity + x.extra_quantity }).ToList();
@@ -313,27 +284,6 @@ namespace CoolCatCollects.Bricklink
 				MinifigValue = minifigValue,
 				PartsValue = partsValue
 			};
-		}
-
-		private GetPriceGuideResponse GetPriceGuide(string type, string set)
-		{
-			var result = _apiService.GetRequest<GetPriceGuideResponse>($"items/{type}/{set}/price");
-
-			return result;
-		}
-
-		private GetCategoryResponse GetCategory(int category_id)
-		{
-			var result = _apiService.GetRequest<GetCategoryResponse>($"categories/{category_id}");
-
-			return result;
-		}
-
-		private GetItemResponse GetItem(string type, string number)
-		{
-			var result = _apiService.GetRequest<GetItemResponse>($"items/{type}/{number}");
-
-			return result;
 		}
 	}
 }
