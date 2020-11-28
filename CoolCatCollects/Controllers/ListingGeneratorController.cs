251b4a20d52ca7b96fcefa65cf9cd122e03c879d
Peter Storey <peter.storey@360resourcing.co.uk>
2020-11-28 15:40:34 +0000

    Can now upload new figs

--- CoolCatCollects/Controllers/ListingGeneratorController.cs
+++ CoolCatCollects/Controllers/ListingGeneratorController.cs
@@ -11,110 +11,188 @@ using System.Linq;
 
 namespace CoolCatCollects.Controllers
 {
-    public class ListingGeneratorController : Controller
-    {
-        // GET: ListingGenerator
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class ListingGeneratorController : Controller
+	{
+		// GET: ListingGenerator
+		public ActionResult Index()
+		{
+			return View();
+		}
 
-        public ActionResult Form()
-        {
-            var model = new ListingGeneratorFormModel();
-            return View(model);
-        }
+		public ActionResult Form()
+		{
+			var model = new ListingGeneratorFormModel();
+			return View(model);
+		}
 
-        public ActionResult ViewTemplate(ListingGeneratorFormModel model)
-        {
-            return View($"~/Views/ListingGenerator/Templates/{model.Type}.cshtml", model);
-        }
+		public ActionResult ViewTemplate(ListingGeneratorFormModel model)
+		{
+			return View($"~/Views/ListingGenerator/Templates/{model.Type}.cshtml", model);
+		}
 
-        [HttpPost]
-        public ActionResult UploadUsedFigures(HttpPostedFileBase file)
-        {
-            var dataSet = GetDataSetFromFile(file.InputStream);
-            var model = DatasetToModel(dataSet);
+		public ActionResult FormResult(ListingGeneratorFormModel model)
+		{
+			model.Title = AddMinifigToTitle(model.Title);
 
-            return View(model);
+			model.Html = Render();
 
-            DataSet GetDataSetFromFile(Stream fileStream)
-            {
-                using (var reader = ExcelReaderFactory.CreateReader(fileStream))
-                {
-                    return reader.AsDataSet();
-                }
-            }
+			return View(model);
 
-            IEnumerable<UsedFigureImportModel> DatasetToModel(DataSet ds)
-            {
-                var arr = new DataRow[ds.Tables[0].Rows.Count];
-                ds.Tables[0].Rows.CopyTo(arr, 0);
-                return arr.Skip(1).Select(x => new UsedFigureImportModel { 
-                    Theme = x.ItemArray[0].ToString(),
-                    SubTheme = x.ItemArray[1].ToString(),
-                    Name = x.ItemArray[2].ToString(),
-                    Number = x.ItemArray[3].ToString(),
-                    Complete = x.ItemArray[4].ToString(),
-                    Condition = x.ItemArray[5].ToString(),
-                    Price = ParseDecimal(x.ItemArray[6])
-                }).ToList();
-            }
+			IHtmlString Render()
+			{
+				var viewLoc = ViewLocation();
+				var viewTemplate = GetViewContents(viewLoc);
 
-            decimal ParseDecimal(object obj)
-            {
-                if (decimal.TryParse(obj.ToString(), out decimal result))
-                {
-                    return result;
-                }
-                return 0;
-            }
-        }
+				var str = !Engine.Razor.IsTemplateCached(viewTemplate, typeof(ListingGeneratorFormModel))
+					? Engine.Razor.RunCompile(viewTemplate, viewTemplate, typeof(ListingGeneratorFormModel),
+						model)
+					: Engine.Razor.Run(viewTemplate, typeof(ListingGeneratorFormModel), model);
 
-        public ActionResult FormResult(ListingGeneratorFormModel model)
-        {
-            model.Title = AddMinifigToTitle(model.Title);
+				return new HtmlString(str.Trim());
+			}
 
-            model.Html = Render();
+			string GetViewContents(string viewTemplate)
+			{
+				return System.IO.File.ReadAllText(Server.MapPath(viewTemplate));
+			}
 
-            return View(model);
+			string ViewLocation()
+			{
+				return $"~/Views/ListingGenerator/Templates/{model.Type}.cshtml";
+			}
 
-            IHtmlString Render()
-            {
-                var viewLoc = ViewLocation();
-                var viewTemplate = GetViewContents(viewLoc);
+			string AddMinifigToTitle(string title)
+			{
+				if (!model.Type.Contains("Fig"))
+				{
+					return title;
+				}
+				if (title.Contains("Minifigure"))
+				{
+					return title;
+				}
 
-                var str = !Engine.Razor.IsTemplateCached(viewTemplate, typeof(ListingGeneratorFormModel))
-                    ? Engine.Razor.RunCompile(viewTemplate, viewTemplate, typeof(ListingGeneratorFormModel),
-                        model)
-                    : Engine.Razor.Run(viewTemplate, typeof(ListingGeneratorFormModel), model);
+				return title + " Minifigure";
+			}
+		}
 
-                return new HtmlString(str.Trim());
-            }
+		#region upload
 
-            string GetViewContents(string viewTemplate)
-            {
-                return System.IO.File.ReadAllText(Server.MapPath(viewTemplate));
-            }
+		[HttpPost]
+		public ActionResult UploadNewFigures(HttpPostedFileBase file)
+		{
+			var dataSet = GetDataSetFromFile(file.InputStream);
+			var model = DatasetToModel(dataSet);
 
-            string ViewLocation()
-            {
-                return $"~/Views/ListingGenerator/Templates/{model.Type}.cshtml";
-            }
+			return View(model);
 
-            string AddMinifigToTitle(string title)
-            {
-                if (!model.Type.Contains("Fig"))
-                {
-                    return title;
-                }
-                if (title.Contains("Minifigure"))
-                {
-                    return title;
-                }
+			DataSet GetDataSetFromFile(Stream fileStream)
+			{
+				using (var reader = ExcelReaderFactory.CreateReader(fileStream))
+				{
+					return reader.AsDataSet();
+				}
+			}
 
-                return title + " Minifigure";
-            }
-        }
-    }
+			IEnumerable<NewFigureImportModel> DatasetToModel(DataSet ds)
+			{
+				var arr = new DataRow[ds.Tables[0].Rows.Count];
+				ds.Tables[0].Rows.CopyTo(arr, 0);
+				return arr.Skip(1).Select(x => new NewFigureImportModel
+				{
+					Theme = x.ItemArray[0].ToString(),
+					SubTheme = x.ItemArray[1].ToString(),
+					Name = x.ItemArray[2].ToString(),
+					Number = x.ItemArray[3].ToString(),
+					Price = ParseDecimal(x.ItemArray[4])
+				}).ToList();
+			}
+
+			decimal ParseDecimal(object obj)
+			{
+				if (decimal.TryParse(obj.ToString(), out decimal result))
+				{
+					return result;
+				}
+				return 0;
+			}
+		}
+
+		[HttpPost]
+		public ActionResult UploadUsedFigures(HttpPostedFileBase file)
+		{
+			var dataSet = GetDataSetFromFile(file.InputStream);
+			var model = DatasetToModel(dataSet);
+
+			return View(model);
+
+			DataSet GetDataSetFromFile(Stream fileStream)
+			{
+				using (var reader = ExcelReaderFactory.CreateReader(fileStream))
+				{
+					return reader.AsDataSet();
+				}
+			}
+
+			IEnumerable<UsedFigureImportModel> DatasetToModel(DataSet ds)
+			{
+				var arr = new DataRow[ds.Tables[0].Rows.Count];
+				ds.Tables[0].Rows.CopyTo(arr, 0);
+				return arr.Skip(1).Select(x => new UsedFigureImportModel
+				{
+					Theme = x.ItemArray[0].ToString(),
+					SubTheme = x.ItemArray[1].ToString(),
+					Name = x.ItemArray[2].ToString(),
+					Number = x.ItemArray[3].ToString(),
+					Complete = x.ItemArray[4].ToString(),
+					Condition = x.ItemArray[5].ToString(),
+					Price = ParseDecimal(x.ItemArray[6])
+				}).ToList();
+			}
+		}
+
+		private decimal ParseDecimal(object obj)
+		{
+			if (decimal.TryParse(obj.ToString(), out decimal result))
+			{
+				return result;
+			}
+			return 0;
+		}
+
+		#endregion
+		#region files
+
+		public ActionResult UsedTemplate()
+		{
+			DownloadFile("~/Files/UsedImportTemplate.xlsx", "UsedFigsImportTemplate.xlsx");
+
+			return Content("");
+		}
+
+		public ActionResult NewTemplate()
+		{
+			DownloadFile("~/Files/NewImportTemplate.xlsx", "NewFigsImportTemplate.xlsx");
+
+			return Content("");
+		}
+
+		/// <summary>
+		/// Downloads a file
+		/// </summary>
+		/// <param name="filePath">Path to file pre-mappath</param>
+		/// <param name="fileName">Filename when downloading</param>
+		private void DownloadFile(string filePath, string fileName)
+		{
+			Response.ClearContent();
+			Response.Clear();
+			Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+			Response.AddHeader("Content-Disposition", $"attachment; filename={fileName};");
+			Response.TransmitFile(Server.MapPath(filePath));
+			Response.Flush();
+			Response.End();
+		}
+
+		#endregion
+	}
 }
